c7b4623dbccf53b4c0f786b6801900b8b6008581
Neta Hadar Itzhak <netook6@gmail.com>
2021-12-13 11:20:24 +0200

    use api.fetchCall instead fetch

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,15 +1,15 @@
-
+import api
+ from "./api";
 export const BASE_URL = "https://register.nomoreparties.co";
 
 export function register({ email, password }) {
-  return fetch(`${BASE_URL}/signup`, {
+  return api.fetchCall(`${BASE_URL}/signup`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ email, password }),
   })
-    .then((res) => res.json())
     .then((data) => {
       if (data.error) {
         throw new Error("User with this email address already exists");
@@ -18,14 +18,13 @@ export function register({ email, password }) {
 }
 
 export function authorize({ email, password }) {
-  return fetch(`${BASE_URL}/signin`, {
+  return api.fetchCall(`${BASE_URL}/signin`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ email, password }),
   })
-    .then((res) => res.json())
     .then((data) => {
       if (data.token) {
         localStorage.setItem("jwt", data.token);
@@ -36,12 +35,11 @@ export function authorize({ email, password }) {
 }
 
 export function getContent(token) {
-    return fetch(`${BASE_URL}/users/me`, {
+    return api.fetchCall(`${BASE_URL}/users/me`, {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
           'Authorization': `Bearer ${token}`,
         }
       })
-      .then(res => res.json())
   } 
