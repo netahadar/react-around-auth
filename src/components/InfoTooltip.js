dd3c979d38c2bb5eb7396e7abf131b8b10f5144e
Neta Hadar Itzhak <netook6@gmail.com>
2021-12-13 11:29:22 +0200

    add alt to image

--- src/components/InfoTooltip.js
+++ src/components/InfoTooltip.js
@@ -8,7 +8,7 @@ export default function InfoTooltip(props) {
         <div className={`popup popup_type_${props.name} ${props.isOpen ? "popup_opened":''}`} >
           <div className="popup__frame">
             <button className="popup__close-button" type="button" onClick={props.onClose}></button>
-            <img src={props.src} className="popup__status-image"></img>
+            <img src={props.src} className="popup__status-image" alt="request status"></img>
             <h2 className="popup__title">{props.title}</h2>
           </div>
         </div>
